7b72cac171f11f10906e04a46532ad60b940f992
LucasVon0645 <lucas.vongarcia@gmail.com>
2025-11-05 19:24:13 -0300

    Remove sensitive information from secrets.h

--- src/secrets.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define WIFI_SSID        "LucasGPoliLock"
-#define WIFI_PASS        "cacarato"
-#define DEVICE_ID        "esp32-01"
-#define BLYNK_TEMPLATE_ID "TMPL2aoP6NrtF"
-#define BLYNK_TEMPLATE_NAME "PoliLock ESP32"
-#define BLYNK_AUTH_TOKEN "n9UQlyAXgD_sgu3Al-sCIRpyWCak0e5i"
